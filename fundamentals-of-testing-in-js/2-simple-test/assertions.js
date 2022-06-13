f24cc48cb6f177814bac8c49c584a45d6863089a
logic-fabric <lowic@logic-fabric.fr>
2022-06-13 07:33:34 +0200

    feature: add expect().toEqual() to assertions.js

--- fundamentals-of-testing-in-js/2-simple-test/assertions.js
+++ fundamentals-of-testing-in-js/2-simple-test/assertions.js
@@ -8,13 +8,27 @@ function expect(output) {
       }
 
       console.error(`    > FAIL: Expected ${output} to be ${expected}`);
-      console.error("---");
-      console.error("Error traceback:");
-      console.error("---");
+      showTraceback();
+    },
 
-      throw new Error();
+    toEqual: (expected) => {
+      if (output == expected) {
+        console.log("    > PASS");
+        return;
+      }
+
+      console.error(`    > FAIL: Expected ${output} to equal ${expected}`);
+      showTraceback();
     },
   };
 }
+
+const showTraceback = () => {
+  console.error("---");
+  console.error("Error traceback:");
+  console.error("---");
+
+  throw new Error();
+};
 
 module.exports = { expect };
